2fc4bed45db17a8ffe0efe09283cc3d32f13227f
Eduardo <eduardo_polanski@hotmail.com>
2021-09-26 20:45:41 -0300

    update readme

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,8 +4,8 @@ import { connect } from "mongoose"
 const url = "mongodb://localhost:27017/loja"
 
 export const mongoConnect = async () => {
+    
     try {
-
         console.log("Conectado ao MongoDB...")
         await connect(url as string)
         console.log("MongoDB conectado com sucesso!")
