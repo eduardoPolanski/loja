0ac7f5a4e9922a36f58188301a090fe3cca0e205
Eduardo <eduardo_polanski@hotmail.com>
2021-09-26 13:49:41 -0300

    second commit

--- src/server.ts
+++ src/server.ts
@@ -53,4 +53,4 @@ app.use((err: Error, request: Request, response:Response, next: NextFunction) =>
 })
 
 //porta do servidor
-app.listen(3333, () => console.log("Serve running port 3333"))
+app.listen(3333, () => console.log("Serve running http://localhost:3333"))
